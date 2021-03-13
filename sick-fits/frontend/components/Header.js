10c8723cd30bb757882515ad9a131bf4f0756b0c
Chindalath <chindalatht@gmail.com>
2021-03-13 11:11:20 -0800

    styles logo

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -1,18 +1,35 @@
-import Link from 'next/link';
-import Nav from './Nav';
+import Link from 'next/link'
+import Nav from './Nav'
+import styled from 'styled-components'
+
+const Logo = styled.h1`
+  font-size: 4rem;
+  margin-left: 2rem;
+  position: relative;
+  z-index: 2;
+  transform: skew(-7deg);
+  background: red;
+  a {
+    color: white;
+    text-decoration: none;
+    text-transform: uppercase;
+    padding: 0.5rem 1rem;
+  }
+`;
+
 
 export default function Header() {
   return (
-    <header>
+    <HeaderStyles>
       <div className="bar">
-        <Link href="/">
-          <h1>Sick Fits</h1>
-        </Link>
+        <Logo>
+          <Link href="/">Sick Fits</Link>
+        </Logo>
       </div>
       <div className="sub-bar">
         <p>Search</p>
       </div>
       <Nav />
-    </header>
+    </HeaderStyles>
   );
 }
