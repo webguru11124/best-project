ee1310c97728e4b9cf9def39e079ca9a2ed4ebf4
Chindalath <chindalatht@gmail.com>
2021-03-13 11:11:32 -0800

    styles header

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -17,6 +17,21 @@ const Logo = styled.h1`
   }
 `;
 
+const HeaderStyles = styled.header`
+  .bar {
+    border-bottom: 10px solid var(--black, black);
+    display: grid;
+    grid-template-columns: auto 1fr;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .sub-bar {
+    display: grid;
+    grid-template-columns: 1fr auto;
+    border-bottom: 1px solid var(--black, black);
+  }
+`;
 
 export default function Header() {
   return (
