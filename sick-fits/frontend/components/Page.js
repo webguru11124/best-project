d601fe9f340477fcfaf7b3a1645a681c9bf3ee45
Chindalath <chindalatht@gmail.com>
2021-03-13 11:48:18 -0800

    styles html and box sizing

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -10,6 +10,22 @@ const GlobalStyles = createGlobalStyle`
     font-style: normal;
 
   }
+  html {
+    --red: #ff0000;
+    --black: #393939;
+    --grey: #3A3A3A;
+    --gray: var(--grey);
+    --lightGrey: #e1e1e1;
+    --lightGray: var(--lightGrey);
+    --offWhite: #ededed;
+    --max-width: 1000px;
+    --bs: 0 12px 24px 0 rgba(0,0,0,0.9);
+    box-sizing: border-box;
+  }
+  *, *:before, *:after {
+    box-sizing: inherit;
+
+  }
   
 `;
 
