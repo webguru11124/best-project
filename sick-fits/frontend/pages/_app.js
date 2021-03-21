00b0756043481cea09e11ee774625982d0ffa673
Chindalath <chindalatht@gmail.com>
2021-03-21 00:17:36 -0700

    styles loading page

--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -1,6 +1,12 @@
-import Page from '../components/Page';
-// TODO: Swap with our own
-import 'nprogress/nprogress.css'
+import NProgress from 'nprogress'
+import Page from '../components/Page'
+import Router from 'next/router'
+
+Router.events.on('routeChangeStart', () => NProgress.start())
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
+
+import '../components/styles/nprogress.css'
 
 export default function MyApp({ Component, pageProps }) {
   return (
