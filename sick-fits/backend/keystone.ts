1d00fa23dbb334dd95fc7017e352fffaf2f88c27
Chindalath <chindalatht@gmail.com>
2021-03-21 00:39:18 -0700

    creates keystone

---
+++ sick-fits/backend/keystone.ts
@@ -0,0 +1,10 @@
+import 'dotenv/config';
+import { config, createSchema } from '@keystone-next/keystone/schema';
+
+const databaseURL =
+  process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
+
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 350,
+  secret: process.env.COOKIE_SECRET,
+};
