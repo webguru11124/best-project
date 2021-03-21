2404f889ff324921262a374e9f53567525ad6d3a
Chindalath <chindalatht@gmail.com>
2021-03-21 00:50:05 -0700

    exports config and adds notes for todos

--- sick-fits/backend/keystone.ts
+++ sick-fits/backend/keystone.ts
@@ -8,3 +8,25 @@ const sessionConfig = {
   maxAge: 60 * 60 * 24 * 350,
   secret: process.env.COOKIE_SECRET,
 };
+
+export default config({
+  server: {
+    cors: {
+      origin: [process.env.FRONTEND_URL],
+      credentials: true,
+    },
+  },
+  db: {
+    adapter: 'mongoose',
+    url: databaseURL,
+    // todo: add database seed data here
+  },
+  lists: createSchema({
+    // schema items go in here
+  }),
+  ui: {
+    //   todo: change this for roles
+    isAccessAllowed: () => true,
+  },
+  //   todo: add session values here
+});
