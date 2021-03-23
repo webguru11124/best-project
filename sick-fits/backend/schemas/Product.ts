bafbf70a56afe6a13446a77d8e224b23eb61ba29
Chindalath <chindalatht@gmail.com>
2021-03-23 15:48:16 -0700

    adds more attributes to products: status, default values, and prices

--- sick-fits/backend/schemas/Product.ts
+++ sick-fits/backend/schemas/Product.ts
@@ -11,5 +11,19 @@ export const Product = list({
         displayMode: 'textarea',
       },
     }),
+    status: select({
+      options: [
+        { label: 'Draft', value: 'DRAFT' },
+        { label: 'Available', value: 'AVAILABLE' },
+        { label: 'Unavailable', value: 'UNAVAILABLE' },
+      ],
+      defaultValue: 'DRAFT',
+      ui: {
+        displayMode: 'segmented-control',
+        createView: { fieldMode: 'hidden' },
+      },
+    }),
+    price: integer(),
+    // todo: photo
   },
 });
